342c73d8cc80c7560940ddff5fefa18124ff20c5
shivam <shivam88629@gmail.com>
2024-10-01 15:52:44 +0530

    updated line

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './layout.jsx'
+import Layout from './Layout.jsx';
 import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
